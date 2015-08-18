7977c708313f94553f114fd40213e097116379bd
Stuart Chapman <stubez@users.noreply.github.com>
2015-08-18 12:03:20 +0100

    Initial project, with Podfile

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'Game of Head Stones' do
+	pod 'SwiftyJSON', '~> 2.2.1'
+end
+
+target 'Game of Head StonesTests' do
+
+end
---
+++ Pods/Target Support Files/Pods-Game of Head Stones/Pods-Game of Head Stones-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Game_of_Head_StonesVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Game_of_Head_StonesVersionString[];
+
